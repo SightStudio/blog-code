5b8444cd217d52ac72ccb90734ce3b204c39b712
SightStudio <sdm3285@gmail.com>
2021-03-17 19:23:38 +0900

    project-init

---
+++ introduce-grpc/grpc-spring-java/src/test/java/com/sight/grpc/GrpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sight.grpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
