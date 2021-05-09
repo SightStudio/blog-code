3bf9ac0f5dca2ffc218ec2105591f679742f697f
sightstudio <sdm3285@gmail.com>
2021-05-10 01:37:24 +0900

    jacoco-init

---
+++ spring-jacoco-start/src/main/java/com/jacoco/sight/SpringJacocoStartApplication.java
@@ -0,0 +1,13 @@
+package com.jacoco.sight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJacocoStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJacocoStartApplication.class, args);
+    }
+
+}
---
+++ spring-jacoco-start/src/test/java/com/jacoco/sight/SpringJacocoStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacoco.sight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJacocoStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
