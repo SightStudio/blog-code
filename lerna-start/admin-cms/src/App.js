9bc8ee8d0ca90bdf02e5a9f49a359446a8881653
SightStudio <sdm3285@gmail.com>
2023-07-02 15:46:56 +0900

    [lerna] lerna로 node에서 멀티모듈 형태로 구성

---
+++ lerna-start/admin-cms/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import HelloSubModule from 'common-module/src/component/HelloSubModule';
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+
+        <HelloSubModule/>
+
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ lerna-start/common-module/src/component/HelloSubModule.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const HelloSubModule = () => {
+  return (
+      <div>this is component from submodule</div>
+  )
+}
+
+export default HelloSubModule;
