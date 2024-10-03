49d3fb300b0552232f6b723a72a2657146dd65bb
SightStudio <sdm3285@gmail.com>
2024-10-03 17:26:25 +0900

    polish. 블로그에 없는 코드들은 모두 제거한다.

--- spring-test-containers/src/main/java/com/test/sight/domain/user/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.test.sight.domain.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
--- spring-test-containers/src/test/java/com/test/sight/config/BaseTest.java
+++
@@ -1,9 +0,0 @@
-package com.test.sight.config;
-
-import java.io.InputStream;
-
-interface BaseTest {
-    default InputStream getResourceAsStream(final String path) {
-        return this.getResourceAsStream(path);
-    }
-}
